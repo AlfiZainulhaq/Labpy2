4cff4f9c2079a9b3fd9617901aa245ead3dc6cbe
alfizainulhaq <alfizainulhaq@gmail.com>
2025-10-23 15:48:27 +0700

    perbaikan latihan1

--- Latihan1.py
+++ Latihan1.py
@@ -1,15 +1,15 @@
-bil1 = int(input("Masukan bilangan 1: ")) 
-bil2 = int(input("Masukan bilangan 2: ")) 
-bil3 = int(input("Masukan bilangan 3: ")) 
-bil4 = int(input("Masukan bilangan 4: ")) 
+a = int(input("Masukan bilangan 1: ")) 
+b = int(input("Masukan bilangan 2: ")) 
+c = int(input("Masukan bilangan 3: ")) 
+d = int(input("Masukan bilangan 4: ")) 
 
-if str(bil1 > bil2 & bil1 > bil3 & bil1 > bil4):
-    terbesar = bil1
-elif str(bil2 > bil1 & bil2 > bil3 & bil2 > bil4):
-    terbesar = bil2
-elif str(bil3 > bil1 & bil3 > bil2 & bil3 > bil4):
-    terbesar = bil3
-else:
-    terbesar = bil4
+if a > b:
+    terbesar = a
+elif b > c:
+    terbesar = b
+elif c > d:
+    terbesar = c
+else: 
+    terbesar = d
 
-print ("Bilangan terbesar adalah:", terbesar)
+print (f"Bilangan terbesar adalah:: {terbesar}")
